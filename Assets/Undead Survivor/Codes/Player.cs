858d239c401e211c22671254375e86ce2e75996b
JinYoung Jang <ending1025@gmail.com>
2023-01-15 22:01:50 +0900

    Animator

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -2,26 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;  
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
-    Rigidbody2D rigid;
     public float speed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
     void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();    
-    }
-
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; //내가 입력한 키보드 좌표를 정규화시키고 프레임보정한값 곱한다.
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }    
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0 )
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+
     }
 }
